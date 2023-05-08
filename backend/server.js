05d053bdcec79bdad0843b61c020977b10f7f6d3
neha-decrypt <neha.decrypt@gmail.com>
2023-05-08 21:26:19 +0530

    push

---
+++ backend/server.js
@@ -0,0 +1,36 @@
+const express = require("express")
+const http = require("http")
+const app = express()
+const cors = require("cors")
+const server = http.createServer(app)
+app.use(cors({
+    origin: "*"
+}))
+
+app.get("/", (req, res) => {
+    res.send("Server is running")
+})
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+})
+
+io.on("connection", (socket) => {
+    socket.emit("me", socket.id)
+
+    socket.on("disconnect", () => {
+        socket.broadcast.emit("callEnded")
+    })
+
+    socket.on("callUser", (data) => {
+        io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+    })
+
+    socket.on("answerCall", (data) => {
+        io.to(data.to).emit("callAccepted", data.signal)
+    })
+})
+
+server.listen(8080, () => console.log("server is running on port 5000"))
