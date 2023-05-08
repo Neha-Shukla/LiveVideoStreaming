31dafe81f24b1222a71640be73df2f7722b899d5
neha-decrypt <neha.decrypt@gmail.com>
2023-05-08 23:17:45 +0530

    push

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,8 @@
 const express = require("express")
-const http = require("http")
+const https = require("https")
 const app = express()
 const cors = require("cors")
-const server = http.createServer(app)
+const server = https.createServer(app)
 app.use(cors({
     origin: "*"
 }))
